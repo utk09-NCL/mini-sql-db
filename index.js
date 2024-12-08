5cd24d9cc9ecdc3d4263fe62a39b8e1114c22472
utk09-NCL <utkarsh.t0298@gmail.com>
2024-12-08 12:22:17 +0000

    feat: updated index.js

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 // index.js
 
-import { logDebug, logError, logInfo, logSuccess } from "./logger/logger.js";
+// Storage
+export { DATABASE_PATH, getTableFilePath, readJSON, writeJSON } from "./lib/storage.js";
 
-logError("This is my error");
-logInfo("My info");
-logSuccess("My success");
-logDebug("My debug");
+// Schema
+export { createTable } from "./lib/schema.js";
+
+// Queries
+export { insertInto, select } from "./lib/query.js";
+
+// Indexing
+export { createIndex, searchWithIndex } from "./lib/indexing.js";
+
+// Backup
+export { backupDatabase, restoreDatabase } from "./lib/backup.js";
+
+// Locks
+export { lockTable, performWithLock } from "./lib/locks.js";
